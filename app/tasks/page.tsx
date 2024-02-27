1117e46a56fe3e4743281a3fde89326864e9accf
IbrahimShehade <ibrahimshehade2020@gmail.com>
2024-02-27 09:52:25 +0200

    force dynamic

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,6 +1,6 @@
 import Taskform from "../ui/taskform";
 import Tasklist from "../ui/tasklist";
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
   return (
     <div className="m-5 ml-10">
